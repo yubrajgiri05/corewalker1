52e81cd8b440225f7b7ed1ad8fe2144bf325a6be
yubrajgiri05 <giriyubraj787898@gmail.com>
2024-08-12 23:14:01 +0545

    responsive

--- src/components/Image_with_elevated_content.tsx
+++ src/components/Image_with_elevated_content.tsx
@@ -4,27 +4,10 @@ import {Image_with_elevated_content_container} from './index_style';
 import Imagewith_elevated_content_text from './Image_with_elevated_content_text'
 
 
-
-const Bg_image_container = styled.div`
-  display: flex;
-  flex-grow: 1;
-  flex-basis: 55%;
-  background-image: url(${props => props.bgImage});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  transform: scale(1.05);
-  transform-origin: left bottom;
-  z-index: -1
-  `;
-
-
 const Image_with_elevated_content = ({ bgImage, title, content,buttonContent}) => {
 
   return (
-    <Image_with_elevated_content_container>
-        <Bg_image_container bgImage={bgImage}>
-        </Bg_image_container>
+    <Image_with_elevated_content_container bgImage={bgImage}>
         <Imagewith_elevated_content_text title={title} content={content} buttonContent={buttonContent}/>
     </Image_with_elevated_content_container>
   )
--- src/components/Service_card.tsx
+++ src/components/Service_card.tsx
@@ -6,22 +6,49 @@ import {Image_with_elevated_content_title,Image_with_elevated_content_content,Bu
 
 export const Service_title = styled( Image_with_elevated_content_title, )`
   text-align: center;
-  font-size: 22px;
+  font-size: 12px;
+  line-height: 20px;
+
+  @media screen and (min-width: 445px) {
+        font-size: 18px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+    line-height: 25px;
+
+  }
+  @media screen and (min-width: 1024px) {
+      font-size: 22px;
+  }
 `;
 export const Service_content = styled( Image_with_elevated_content_content, )`
   color: white;
-  font-size: 18px;
+  font-size: 15px;
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 5;
-  line-clamp: 5;
+  -webkit-line-clamp: 4;
+  line-clamp: 4;
   -webkit-box-orient: vertical;
+  line-height: 20px;
+
+  @media screen and (min-width: 1024px) {
+      font-size: 18px;
+      line-height: 25px;
+
+  }
 `;
 export const Service_title_onhover = styled( Image_with_elevated_content_title, )`
   color: white;
-  font-size: 22px;
-  padding-bottom: 15px;
+  font-size: 20px;
+  padding-bottom: 10px;
+  line-height: 25px;
+
+  @media screen and (min-width: 1024px) {
+      font-size: 22px;
+      line-height: 35px;
+
+  }
 `;
 export const Service_button = styled( Button, )`
   color: white;
@@ -33,7 +60,7 @@ const Service_card = ({title,content,buttonContent}) => {
   return (
     <Service_container>
       <Service_box>
-        <Image src="https://www.svgrepo.com/show/340190/dns-services.svg" alt="logo" width={100} height={100} />
+        <Image src="https://www.svgrepo.com/show/340190/dns-services.svg" alt="logo" width={60} height={60} />
         <Service_title>
           {title}
         </Service_title>
